ad6232f0092c22ffa442893e30ce3434c632b98a
rahulxsh <rkbagra0490@gmail.com>
2026-05-28 01:04:05 +0530

    Added 10 largest files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-use std::ffi::OsStr;
+use std::collections::HashMap;
+use std::ffi::{OsStr, OsString};
 use std::fs::Permissions;
 use std::path::{Path, PathBuf};
 use std::time::SystemTime;
 use walkdir::WalkDir;
 
 fn main() {
-    let path = Path::new("./src");
+    let path = Path::new("/Users/rahulsharma/Projects/aeroamenities-backend");
     scan(path);
 }
 
@@ -13,9 +14,10 @@ fn scan(path: &Path) {
     let mut total_file = 0;
     let mut total_dir = 0;
     let mut files_info:Vec<FileInfo> = Vec::new();
+    let mut extensions_count:HashMap<OsString,u64> = HashMap::new();
 
-    for entry in WalkDir::new(path) {
-        let entry = match entry {
+    for entry_dir in WalkDir::new(path) {
+        let entry_dir = match entry_dir {
             Ok(e) => e,
             Err(err) => {
                 eprintln!("Error: {}", err);
@@ -23,13 +25,13 @@ fn scan(path: &Path) {
             }
         };
 
-        let file_type = entry.file_type();
-        let metadata = entry.metadata();
+        let file_type = entry_dir.file_type();
+        let metadata = entry_dir.metadata();
 
         if file_type.is_file() {
             if let Ok(value) = metadata {
                 let f_info = FileInfo {
-                    path:entry.path().to_path_buf(),
+                    path:entry_dir.path().to_path_buf(),
                     size:value.len(),
                     created:value.created().ok(),
                     modified:value.modified().ok(),
@@ -43,16 +45,17 @@ fn scan(path: &Path) {
 
         if file_type.is_dir() {
             total_dir += 1;
-            println!("[DIR] {}", entry.path().display());
+            println!("[DIR] {}", entry_dir.path().display());
         }
 
         if file_type.is_file() {
             total_file += 1;
-            println!("[FILE] {}", entry.path().display());
+            println!("[FILE] {}", entry_dir.path().display());
 
-            if let Some(ext) = entry.path().extension() {
+            if let Some(ext) = entry_dir.path().extension() {
+                *extensions_count.entry(ext.to_os_string()).or_insert(0) += 1;
                 if ext == OsStr::new("rs") {
-                    println!("Rust File -> {}", entry.path().display());
+                    println!("Rust File -> {}", entry_dir.path().display());
                 }
             }
         }
@@ -62,9 +65,26 @@ fn scan(path: &Path) {
     println!("Total dirs: {}", total_dir);
 
     if !files_info.is_empty(){
-        for i in files_info {
-            println!("{:?} \n",i);
+        files_info.sort_by(|a,b| b.size.cmp(&a.size) );
+
+        if let Some(file) = files_info.first() {
+            println!("\n Largest File:{:?} \n\n",file);
         }
+
+        for (index, file) in files_info.iter().take(10).enumerate() {
+            println!(
+                "{}. {} -> {} bytes",
+                index + 1,
+                file.path.display(),
+                file.size
+            );
+        }
+
+    }
+
+
+    for (exten,count) in extensions_count {
+        println!("{:?} -> {}",exten,count);
     }
 }
 
