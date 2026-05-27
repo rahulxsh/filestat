4872f0d8f59feb6c9639f2adb2d55941608cdbd1
rahulxsh <rkbagra0490@gmail.com>
2026-05-28 00:04:36 +0530

    Get file metadata

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::ffi::OsStr;
-use std::path::Path;
+use std::fs::Permissions;
+use std::path::{Path, PathBuf};
+use std::time::SystemTime;
 use walkdir::WalkDir;
 
 fn main() {
@@ -10,6 +12,7 @@ fn main() {
 fn scan(path: &Path) {
     let mut total_file = 0;
     let mut total_dir = 0;
+    let mut files_info:Vec<FileInfo> = Vec::new();
 
     for entry in WalkDir::new(path) {
         let entry = match entry {
@@ -21,6 +24,22 @@ fn scan(path: &Path) {
         };
 
         let file_type = entry.file_type();
+        let metadata = entry.metadata();
+
+        if file_type.is_file() {
+            if let Ok(value) = metadata {
+                let f_info = FileInfo {
+                    path:entry.path().to_path_buf(),
+                    size:value.len(),
+                    created:value.created().ok(),
+                    modified:value.modified().ok(),
+                    permissions:value.permissions(),
+                    accessed:value.accessed().ok()
+                };
+
+                files_info.push(f_info);
+            }
+        }
 
         if file_type.is_dir() {
             total_dir += 1;
@@ -41,4 +60,20 @@ fn scan(path: &Path) {
 
     println!("Total files: {}", total_file);
     println!("Total dirs: {}", total_dir);
+
+    if !files_info.is_empty(){
+        for i in files_info {
+            println!("{:?} \n",i);
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct FileInfo {
+    pub path:PathBuf,
+    pub size: u64,
+    pub created:Option<SystemTime>,
+    pub permissions: Permissions,
+    pub accessed:Option<SystemTime>,
+    pub modified:Option<SystemTime>
 }
