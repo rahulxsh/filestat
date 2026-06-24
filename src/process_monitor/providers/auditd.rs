b5e1c5b2f6ae55cda4a33ffc207920e217758431
rahulxsh <rkbagra0490@gmail.com>
2026-06-24 22:19:01 +0530

    Added event id extractor function

--- src/process_monitor/providers/auditd.rs
+++ src/process_monitor/providers/auditd.rs
@@ -9,17 +9,23 @@ pub fn auditd_provider() -> Result<()> {
     let mut reader = BufReader::new(file);
 
     reader.seek(SeekFrom::End(0));
+    let mut map:HashMap<u64,Vec<HashMap<String,String>>> = HashMap::new();
 
     loop {
         let mut line = String::new();
 
         if reader.read_line(&mut line)? > 0 {
             let fields = parse_fields(&line);
-            println!("Fields is:{:?}",fields);
+            let event_id = event_id(&line);
+
+            if let Some(id) = event_id {
+                map.entry(id).or_default().push(fields)
+            }
         }
         line.clear();
 
         std::thread::sleep(std::time::Duration::from_millis(100));
+        println!("Process Logs:{:?}",map);
     }
 }
 
@@ -36,4 +42,16 @@ fn parse_fields(line:&str) -> HashMap<String,String> {
         }
     }
     map
+}
+
+fn event_id(line: &str) -> Option<u64> {
+    let start = line.find("audit(")?;
+    let rest = &line[start + 6..];
+
+    let colon = rest.find(':')?;
+    let after_colon = &rest[colon + 1..];
+
+    let end = after_colon.find(')')?;
+
+    after_colon[..end].parse().ok()
 }
